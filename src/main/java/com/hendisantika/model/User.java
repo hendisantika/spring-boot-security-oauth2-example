e705fe33e29b58b9f4c153e8d65aa42577180837
Hendi Santika <hendisantika@gmail.com>
2017-12-31 19:42:30 +0700

    #3 - Add User Entity

---
+++ src/main/java/com/hendisantika/model/User.java
@@ -0,0 +1,78 @@
+package com.hendisantika.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-oauth2-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/12/17
+ * Time: 15.45
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    @JsonIgnore
+    private String password;
+
+    @Column
+    private long salary;
+
+    @Column
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
