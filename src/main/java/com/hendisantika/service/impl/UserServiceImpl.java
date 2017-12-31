c96b7c30e31d7c7a182d86d900bfe4b70cbd81eb
Hendi Santika <hendisantika@gmail.com>
2017-12-31 19:44:56 +0700

    #6 - Add User Service Implementation

---
+++ src/main/java/com/hendisantika/service/impl/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package com.hendisantika.service.impl;
+
+import com.hendisantika.dao.UserDao;
+import com.hendisantika.model.User;
+import com.hendisantika.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-oauth2-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/12/17
+ * Time: 15.55
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service(value = "userService")
+public class UserServiceImpl implements UserDetailsService, UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(userId);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(String.valueOf(user.getId()), user.getPassword(), getAuthority());
+    }
+
+    private List<SimpleGrantedAuthority> getAuthority() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        userDao.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    @Override
+    public void delete(long id) {
+        userDao.delete(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userDao.save(user);
+    }
+}
