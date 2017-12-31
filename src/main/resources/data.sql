391cb7ffe43f585211f20d8a85dd89ffe4e1ad00
Hendi Santika <hendisantika@gmail.com>
2017-12-31 19:50:17 +0700

    #11 - Add data.sql script for initialization data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO User (id, username, password, salary, age) VALUES (1, 'hendi', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 3456, 33);
+INSERT INTO User (id, username, password, salary, age) VALUES (2, 'sasuke', '$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK', 7823, 23);
+INSERT INTO User (id, username, password, salary, age) VALUES (3, 'naruto', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 45);
