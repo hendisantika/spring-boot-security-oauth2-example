f067a709c3cbe94067ef8875a999635eb8a440c7
Hendi Santika <hendisantika@gmail.com>
2017-12-31 19:48:20 +0700

    #9 - Add Authorization Server Config

---
+++ src/main/java/com/hendisantika/config/AuthorizationServerConfig.java
@@ -0,0 +1,63 @@
+package com.hendisantika.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-oauth2-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/12/17
+ * Time: 16.06
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    static final String CLIEN_ID = "hendi-client";
+    static final String CLIENT_SECRET = "hendi-secret";
+    static final String GRANT_TYPE = "password";
+    static final String SCOPE_READ = "read";
+    static final String SCOPE_WRITE = "write";
+    static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1 * 60 * 60;
+    static final int FREFRESH_TOKEN_VALIDITY_SECONDS = 6 * 60 * 60;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private UserApprovalHandler userApprovalHandler;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
+
+        configurer
+                .inMemory()
+                .withClient(CLIEN_ID)
+                .secret(CLIENT_SECRET)
+                .authorizedGrantTypes(GRANT_TYPE)
+                .scopes(SCOPE_READ, SCOPE_WRITE)
+                .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS).
+                refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
+                .authenticationManager(authenticationManager);
+    }
+}
