02946c64641f82c069d6b0893e884c83779c8540
Hendi Santika <hendisantika@gmail.com>
2017-12-31 19:41:38 +0700

    #2 - Add Main Apps

---
+++ src/main/java/com/hendisantika/Application.java
@@ -0,0 +1,24 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-oauth2-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/12/17
+ * Time: 15.43
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
