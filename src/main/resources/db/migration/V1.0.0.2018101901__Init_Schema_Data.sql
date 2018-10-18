1227ffd187d4bca65dc8daafbed7699f5a26926f
Hendi Santika <hendisantika@gmail.com>
2018-10-19 06:34:21 +0700

    #29 - Bug Fix flyway migration script and bug fix show sql

---
+++ src/main/resources/db/migration/V1.0.0.2018101901__Init_Schema_Data.sql
@@ -0,0 +1,19 @@
+CREATE TABLE User (
+  id bigint(20) NOT NULL,
+  username varchar(25) NOT NULL,
+  password varchar(75) NOT NULL,
+  salary bigint(10) NOT NULL,
+  age int(2) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO User (id, username, password, salary, age) VALUES (1, 'hendi', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 3456, 33);
+INSERT INTO User (id, username, password, salary, age) VALUES (2, 'sasuke', '$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK', 7823, 23);
+INSERT INTO User (id, username, password, salary, age) VALUES (3, 'naruto', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 45);
+INSERT INTO User (id, username, password, salary, age) VALUES (4, 'sakura', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 40);
+
+
+-- hendi / password
+-- sasuke / password
+-- naruto / password
+-- sakura / password
