0d950fcb9147e369c54ae59dd2c7a66475c1d227
Hendi Santika <hendisantika@gmail.com>
2018-10-19 06:11:49 +0700

    #28 - Add lombok style & flyway migration script

--- src/main/java/com/hendisantika/model/User.java
+++ src/main/java/com/hendisantika/model/User.java
@@ -1,6 +1,7 @@
 package com.hendisantika.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 
 import javax.persistence.*;
 
@@ -15,7 +16,7 @@ import javax.persistence.*;
  * To change this template use File | Settings | File Templates.
  */
 
-
+@Data
 @Entity
 public class User {
 
@@ -36,43 +37,4 @@ public class User {
     @Column
     private int age;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
